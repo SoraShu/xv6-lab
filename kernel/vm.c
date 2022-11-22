2ff2e7e06d345fabcbdce18349f101f129554425
SoraShu <sorashu@outlook.com>
2022-11-22 17:09:01 +0800

    add vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -450,3 +450,35 @@ test_pagetable()
   uint64 gsatp = MAKE_SATP(kernel_pagetable);
   return satp != gsatp;
 }
+
+void
+_vmprint(pagetable_t pagetable, int level)
+{
+  // there are 2^9 = 512 PTEs in a page table.
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+      // this PTE points to a lower-level page table.
+      uint64 child = PTE2PA(pte);
+      // print ||
+      for (int j = 0; j <= level; j++) {
+        printf("||");
+        if (j != level) {
+          printf(" ");
+        }
+      }
+      printf("%d: pte %p pa %p\n", i, pte, child);
+      _vmprint((pagetable_t) child, level+1);
+    } else if (pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+      printf("|| || ||%d: pte %p pa %p\n", i, pte, child);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  _vmprint(pagetable, 0);
+}
